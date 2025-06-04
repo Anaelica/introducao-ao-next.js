3ece8378c4b9ab6d5b3a82720795717526fc6b24
Anaelica <anaanaelica@gmail.com>
2025-06-04 20:43:08 -0300

    feat: Cards

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,4 +1,5 @@
 // import Page from "@/server-client/page";
+import { Cards } from "./components/Cards";
 import NavBar from "./components/Navbar";
 
 export default function Home() {
@@ -16,6 +17,31 @@ export default function Home() {
       {/* <Page/> */}
       </div>
     </div>
+     <section className="bg-white py-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+        <h2 className="text-3xl font-bold text-center text-gray-900 mb-12">
+          Recursos Principais
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8" >
+          <Cards
+            title="Renderização Híbrida"
+            description="SSG, SSR e ISR para otimizar performance e SEO."
+            icon="🚀" />
+
+          <Cards
+            title="CSS Utility-First"
+            description="Tailwind CSS para estilização rápida e consistente."
+            icon="🎨" />
+
+          <Cards
+            title="TypeScript Integrado"
+            description="Tipagem estática para um código mais seguro."
+            icon="🔒" />
+        </div>
+
+      </section>
       </>
   )
 }
+
